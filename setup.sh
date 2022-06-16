07f979511929965e2c5ae41d45b38956c101ef86
Nawwar Kasrawi <89160167+nkasrawi@users.noreply.github.com>
2022-06-16 03:17:06 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 REGION=$1
 
-aws --region $REGION cloudformation package --template pipeline.yml  --s3-bucket sxm-codebuild-$REGION-$APPLICATIONNAME  --output-template-file pipeline-export.yml
-aws --region $REGION cloudformation deploy --template-file pipeline-export.yml --stack-name $APPLICATIONNAME-$PROJECTID-pipeline --parameter-overrides \
+aws --region $REGION cloudformation package --template cloud9-template.yml  --s3-bucket nkasrawi-cloud9-$REGION  --output-template-file cloud9-template-export.yml
+aws --region $REGION cloudformation deploy --template-file cloud9-template-export.yml --stack-name nkasrawi-cloud9 
