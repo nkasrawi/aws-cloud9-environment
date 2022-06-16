78a55cd100740e45515c495f4de83730bad49737
Nawwar Kasrawi <89160167+nkasrawi@users.noreply.github.com>
2022-06-16 03:24:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 REGION=$1
+VPC=$2
 
 aws --region $REGION cloudformation package --template cloud9-template.yml  --s3-bucket nkasrawi-cloud9-$REGION  --output-template-file cloud9-template-export.yml
-aws --region $REGION cloudformation deploy --template-file cloud9-template-export.yml --stack-name nkasrawi-cloud9 
+aws --region $REGION cloudformation deploy --template-file cloud9-template-export.yml --stack-name nkasrawi-cloud9 --parameter-overrides \
+	VpcName=$VPC
