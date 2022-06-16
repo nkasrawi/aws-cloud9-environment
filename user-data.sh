0a060d4f68c674b3ad51510f5728aed450c1dbb6
nkasrawi <nawwar@kasrawi.org>
2022-06-16 04:16:34 -0400

    Update

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,4 @@
 sudo yum update -y
-sudo yum install zsh
-sudo passwd ec2-user
-chsh -s /bin/zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git config --global credential.helper store
+git config --global alias.st status
+
