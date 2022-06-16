ae1c2e555565a014470ee8c5da23c8aa0bffe177
nkasrawi <nawwar@kasrawi.org>
2022-06-16 04:17:50 -0400

    Update

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,5 @@
 sudo yum update -y
 git config --global credential.helper store
 git config --global alias.st status
-
+cd repos
+git clone https://github.siriusxm.com/SSP/mountain-configuration.git
