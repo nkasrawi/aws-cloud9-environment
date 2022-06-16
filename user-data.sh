59a8e9060a574d8a27fceb0df11fcb738306c5e7
nkasrawi <nawwar@kasrawi.org>
2022-06-16 04:23:13 -0400

    Update

--- user-data.sh
+++ user-data.sh
@@ -3,3 +3,16 @@ git config --global credential.helper store
 git config --global alias.st status
 cd ~/environment/repos
 git clone https://github.siriusxm.com/SSP/mountain-configuration.git
+git clone https://github.siriusxm.com/SSP/heimdall-configuration.git
+git clone https://github.siriusxm.com/SSP/orpheus-configuration.git
+git clone https://github.siriusxm.com/SSP/hades-configuration.git
+git clone https://github.siriusxm.com/SSP/thor-configuration.git
+git clone https://github.siriusxm.com/SSP/acheron-configuration.git
+git clone https://github.siriusxm.com/SSP/scorpius-configuration.git
+git clone https://github.siriusxm.com/SSP/minervams-live-configuration.git
+git clone https://github.siriusxm.com/SSP/minervams-nl-configuration.git
+git clone https://github.siriusxm.com/SSP/cygnus-configuration.git
+git clone https://github.siriusxm.com/SSP/fornax-configuration.git
+git clone https://github.siriusxm.com/SSP/Thanos-Configurations.git
+git clone https://github.siriusxm.com/SSP/medusa.git
+git clone https://github.siriusxm.com/SSP/mountain-360.git
